21416d793b30818fee5f0e4cfed2774c05ffb433
Suhaib <suhaib92khan@gmail.com>
2020-06-10 19:59:33 -0400

    Add CDN's for styling

--- src/static/js/soda.js
+++ src/static/js/soda.js
@@ -11,7 +11,17 @@ $(document).ready(function () {
       style: "multi",
       selector: "td:first-child",
     },
-    data: [["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0]],
+    data: [
+      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
+      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
+      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
+      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
+      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
+      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
+      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
+      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
+      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
+    ],
     "scrollY": "200px",
     columns: [
       { title: "" },
@@ -26,5 +36,3 @@ $(document).ready(function () {
     ],
   });
 });
-
-console.log("wassup")
