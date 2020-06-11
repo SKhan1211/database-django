cff64cb7fdb8bb455d186574a98a11ffe6cc6a27
Suhaib <suhaib92khan@gmail.com>
2020-06-10 22:30:41 -0400

    Add api call and more database entries

--- src/static/js/soda.js
+++ src/static/js/soda.js
@@ -1,38 +1,34 @@
-$(document).ready(function () {
-  $('#table_id').DataTable({
-    columnDefs: [
-      {
-        orderable: false,
-        className: "select-checkbox",
-        targets: 0,
+$.ajax({
+  url: '/api/v1/sodas/',
+}).then(soda => {
+  return soda.map(el => {el['id'] = ''; return Object.values(el)});
+}).then(newSodas => {
+  return $(document).ready(function () {
+    $('#table_id').DataTable({
+      columnDefs: [
+        {
+          orderable: false,
+          className: "select-checkbox",
+          targets: 0,
+        },
+      ],
+      select: {
+        style: "multi",
+        selector: "td:first-child",
       },
-    ],
-    select: {
-      style: "multi",
-      selector: "td:first-child",
-    },
-    data: [
-      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
-      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
-      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
-      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
-      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
-      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
-      ["", 1, "MTNDEW_TRN", "Mountain Dew", "earmdbl101", "eamtrn23", "GREEN", "00:09:42", 0],
-      ["", 2, "PEPSI_TRN", "Pepsi", "eamdbl117", "eamtrn23", "GREEN", "00:08:21", 1],
-      ["", 3, "COKE_TRN", "Coke", "earmdbl145", "eamtrn67", "GREEN", "00:06:23", 0],
-    ],
-    "scrollY": "200px",
-    columns: [
-      { title: "" },
-      { title: "Order" },
-      { title: "Tenant Id" },
-      { title: "Display Name" },
-      { title: "DB Host" },
-      { title: "DB Instance" },
-      { title: "Status" },
-      { title: "Time" },
-      { title: "Errors" },
-    ],
+      data: newSodas,
+      "scrollY": "200px",
+      columns: [
+        { title: "" },
+        { title: "Order" },
+        { title: "Tenant Id" },
+        { title: "Display Name" },
+        { title: "DB Host" },
+        { title: "DB Instance" },
+        { title: "Status" },
+        { title: "Time" },
+        { title: "Errors" },
+      ],
+    });
   });
 });
